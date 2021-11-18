76c53cfd6cdaf4555841a4515474bb3321a40f4f
Adrian <eadepto@hotmail.com>
2021-11-18 11:05:37 -0500

    Clases

--- 01-javascript/02-arreglos.js
+++ 01-javascript/02-arreglos.js
@@ -1,2 +1,26 @@
 // 02-arreglos.js
-const arreglo = [6, 7, 8, 9, 10];
+let arreglo = [6, 7, 8, 9, 10];
+arreglo = 1;
+arreglo = true;
+arreglo = undefined;
+arreglo = null;
+arreglo = {};
+arreglo = [true, 1, 1.1, "Adrian", 'Vicente', undefined, null, {}, [1, 2]];
+arreglo = [6, 7, 8, 9, 10];
+
+// for of
+for (let numero of arreglo) { // VALORES
+    console.log('numero', numero);
+}
+// for in
+for (let indice in arreglo) { // INDICES
+    arreglo[indice];
+    console.log('indice', indice);
+}
+let objetoPrueba = {a: '1', b: '2', c: '3'};
+for (let llave in objetoPrueba) { // INDICES
+    console.log('llave', llave);
+}
+
+
+
