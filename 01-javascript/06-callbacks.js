ab4150a824212c8bdf5ece1f299e7948ddfe07f9
Adrian <eadepto@hotmail.com>
2021-11-30 07:27:59 -0500

    update/clase 09

--- 01-javascript/06-callbacks.js
+++ 01-javascript/06-callbacks.js
@@ -1,1 +1,26 @@
 // 06-callbacks.js
+const fs = require('fs'); // file system
+// 06-ejemplo.txt -> Hola
+console.log('PRIMERO');
+fs.readFile(
+    './06-ejemplo.txt', // 1
+    'utf-8',
+    (error, contenido) => {
+        if(error){
+            console.error({mensaje:'error leyendo contenido 06 ejemplo txt', error: error});
+        }else{
+            fs.readFile(
+                './01-variable.js', // 2
+                'utf-8',
+                (errorVariable, contenidoVariable) => {
+                    if(errorVariable){
+                        console.error({mensaje:'error leyendo contenido 01 variable', error: errorVariable});
+                    }else{
+                        console.log(contenido, contenidoVariable);
+                    }
+                }
+            );
+        }
+    }
+);
+console.log('TERCERO');
