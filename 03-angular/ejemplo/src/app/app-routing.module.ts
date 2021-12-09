17296f58bb8ac20d3f71fd5cab9143e2d4a6c61d
Adrian <eadepto@hotmail.com>
2021-12-09 10:25:54 -0500

    update/ ruta login

--- 03-angular/ejemplo/src/app/app-routing.module.ts
+++ 03-angular/ejemplo/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {RutaLoginComponent} from "./rutas/ruta-login/ruta-login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: RutaLoginComponent,
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+    routes,
+    {useHash: true}
+  )],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
